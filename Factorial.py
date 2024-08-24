d47a2e8c16b3a9a43e4a54fcaaec59ef0ceb6f37
Arpit Kumar <154455719+arpitvirus@users.noreply.github.com>
2024-08-24 15:32:54 +0530

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,18 @@
+# Factorial Calculator
+# Problem: Computer the factorial f a number using  While loop.
+number = 5
+i= 1
+while number > 0:
+    factorial = factorial* number
+    number = number-1
+print(factorial)
+
+# Second method by using while loop >>>>>>>>>
+# number = 5
+# factorial= 1
+# i =1
+# while i<=number:
+#     factorial = factorial* i
+#     i+=1
+
+# print(factorial)
